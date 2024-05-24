07dfeb6efa6c975caa3716be3a74ec1093dbc4cd
priya2614 <165402467+priya2614@users.noreply.github.com>
2024-05-24 18:54:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,105 @@
+//  import './App.css';
+import Bootstrap from "./Bootsrap";
+
+
+function App() {
+  return (
+
+       <>
+       <div>
+        <Bootstrap></Bootstrap>
+       </div>
+
+
+
+
+
+          {/* <h1 style={{textAlign :  'center',
+            color : 'white',
+            backgroundColor : 'blue',
+            borderRadius : '15px',
+            margin : '20px'
+          }}><i>Hyderabad Famous Pl@ces...</i></h1>
+          <p className='para'> Hyderabad is the capital and largest city of the Indian state of Telangana. It occupies 650 km2 (250 sq mi) on the Deccan Plateau along the banks of the Musi River, in the northern part of Southern India. With an average altitude of 542 m (1,778 ft), much of Hyderabad is situated on hilly terrain around artificial lakes, including the Hussain Sagar lake, predating the city's founding, in the north of the city centre. According to the 2011 census of India, Hyderabad is the fourth-most populous city in India with a population of 6.9 million residents within the city limits, and has a population of 9.7 million residents in the metropolitan region, making it the sixth-most populous metropolitan area in India. With an output of US$74 billion, Hyderabad has the fifth-largest urban economy in India.
+          </p>
+          <h2 style={{color : 'purple',
+            margin : '20px'
+          }}>Charminar</h2>
+          <img className='img'
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkn9sYulhShH6_IhUnluNyW5B-NhVcT6WBRWBfDyH_yg&s"
+              alt="image"
+            />
+          
+          <p className='para'>Charminar is one of the oldest monuments in India, and it is situated in
+            Hyderabad, Telangana. Charminar was built in 1591 and is a symbol of
+            Hyderabad. This monument is the official emblem of Telangana. Charminar
+            is derived from Urdu, meaning 'Four Pillars.
+            
+          </p>
+          
+          <h2 style={{color : 'purple',
+            margin : '20px'}} > Golconda Fort</h2>
+          
+            <img className='img'
+              src="https://apnayatra.com/wp-content/uploads/2024/02/Golconda-Fort-Hyderabad.jpg"
+              alt="image" 
+            />
+         
+          <p className='para'>
+            Golconda was the principal capital of the Qutub Shahi kings. The inner
+            fort contains ruins of palaces, mosques and a hill top pavilion, which
+            rises about 130 meters high and gives a bird's eye view of other
+            buildings. Golconda fort is undoubtedly one of most magnificent fortress
+            complexes in India.
+          </p>
+    
+          <h2 style={{color : 'purple',
+            margin : '20px'}}>Birla Mandir </h2>
+          
+            <img className='img'
+              src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0a/56/ff/7a/photo0jpg.jpg?w=1200&h=-1&s=1"
+              alt="image"
+            />
+         
+          <p className='para'>
+            The Birla Mandir complex also houses a temple dedicated to Buddha along
+            with fresco paintings describing his life. The temple also has separate
+            shrines for other gods such as Shiva, Ganesh, Saraswati, Hanuman,
+            Brahma, Lakshmi and Saibaba. In the evening, Birla Mandir presents a
+            spectacular sight upon illumination.
+          </p>
+    
+      
+    
+          <h2 style={{color : 'purple',
+            margin : '20px'}}>Hussain Sagar</h2>
+          
+            <img className='img'
+              src="https://www.holidify.com/images/cmsuploads/compressed/1280px-Hussain_Sagar_lake2C_Hyderabad_20230309153045.jpg"
+              alt="image"
+            />
+       
+          <p className='para'>
+            The lake is man-made and one of the largest in Asia. A monolith Buddha
+            statue was installed in the middle of the lake, which is one of the most
+            sought-after things in the lake. It is also known as the Hussain Sagar
+            Buddha statue.Boating and other water sports are a common feature at
+            Hussain Sagar including regular yachting and activities by sailing
+            clubs. The annual regatta here draws many sports persons from all over
+            the country. Telangana tourism provides facilities like 48 seater Launch
+            etc, speedboats, and motor boats. Starlit dinners and private parties
+            are arranged on the Launch after prior booking.
+          </p> 
+          */}
+          </>
+      );
+    }
+    
+    export default App;
+
+
+
+
+
+
+
